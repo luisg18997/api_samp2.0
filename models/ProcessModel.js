e56a8f0462e01d71f0b315b8034b7515ecf77fac
denis osuna <denisosuna@gmail.com>
2018-10-20 05:37:27 +0000

    Upload New File

---
+++ models/ProcessModel.js
@@ -0,0 +1,74 @@
+const appName = 'ProcessModel';
+const debug = require('debug')(appName);
+const util = require('util');
+
+const pool = require('./pgmodel.js');
+
+
+
+const getProcessFormMovementPersonalList  = (callback) => {
+  const query = util.format('SELECT employee_data.get_process_form_movement_personal_list() as result;');
+  const data = {};
+  return pool.query(query, (err, res) => {
+ if (!err) {
+      debug('res.rows: ', res.rows[0].result.length);
+      if ((res.rowCount !== 0) && (res.rows[0].result != null)) {
+        debug('result obtain rowCount: ', res.rowCount);
+        callback(false, res.rows[0].result);
+      } else {
+        data.result = 'not found';
+        callback(false, data);
+      }
+    } else {
+      callback(err.stack, null);
+    }
+  });
+};
+
+
+const getProcessFormOficeList  = (callback) => {
+  const query = util.format('SELECT employee_data.get_process_form_ofice_list() as result;');
+  const data = {};
+  return pool.query(query, (err, res) => {
+ if (!err) {
+      debug('res.rows: ', res.rows[0].result.length);
+      if ((res.rowCount !== 0) && (res.rows[0].result != null)) {
+        debug('result obtain rowCount: ', res.rowCount);
+        callback(false, res.rows[0].result);
+      } else {
+        data.result = 'not found';
+        callback(false, data);
+      }
+    } else {
+      callback(err.stack, null);
+    }
+  });
+};
+
+
+const getStatusProcessFormList  = (callback) => {
+  const query = util.format('SELECT employee_data.get_status_process_form_list() as result;');
+  const data = {};
+  return pool.query(query, (err, res) => {
+ if (!err) {
+      debug('res.rows: ', res.rows[0].result.length);
+      if ((res.rowCount !== 0) && (res.rows[0].result != null)) {
+        debug('result obtain rowCount: ', res.rowCount);
+        callback(false, res.rows[0].result);
+      } else {
+        data.result = 'not found';
+        callback(false, data);
+      }
+    } else {
+      callback(err.stack, null);
+    }
+  });
+};
+
+module.exports = {
+
+  getProcessFormMovementPersonalList,
+  getProcessFormOficeList,
+  getStatusProcessFormList
+
+};

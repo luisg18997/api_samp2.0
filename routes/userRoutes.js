db2b68daf804194e485782d993b7fd24cb11d7de
denis osuna <denisosuna@gmail.com>
2018-10-21 10:12:30 +0000

    Upload New File

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const UserRouter = express.Router();
+
+const UserController = require('../controllers/userController.js');
+
+UserRouter.post('/Roles', UserController.getAllRolesList);
+
+UserRouter.post('/SecurityAnswer/SecurityAnswerFilterQuestion', UserController.getAllSecurityAnswerFilterQuestionList);
+
+UserRouter.post('/SecurityAnswer', UserController.getAllSecurityAnswerList);
+
+UserRouter.post('/SecurityQuestions', UserController.getAllSecurityQuestionsList);
+
+UserRouter.post('/Ubications', UserController.getAllUbicationsList);
+
+UserRouter.post('/Roles/UserRole', UserController.getAllUserRoleList);
+
+module.exports = userRouter;

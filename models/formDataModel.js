1191fb23659b0f6f873158dac9342d67a46f8c74
lgonzalez <luis@ezgmz.com>
2018-11-12 13:20:29 -0400

    add new model of add ofice

--- models/formDataModel.js
+++ models/formDataModel.js
@@ -23,6 +23,26 @@ const getMovementTypeslist = (callback) => {
   });
 };
 
+const addNewOfice = (employee, formOfice, callback) => {
+  const query = util.format('SELECT form_data.employee_form_ofice_insert_complete() as result;');
+  const data = {};
+  return pool.query(query, (err, res) => {
+ if (!err) {
+      debug('res.rows: ', res.rows[0].result.length);
+      if ((res.rowCount !== 0) && (res.rows[0].result != null)) {
+        debug('result obtain rowCount: ', res.rowCount);
+        callback(false, res.rows[0].result);
+      } else {
+        data.result = 'not found';
+        callback(false, data);
+      }
+    } else {
+      callback(err.stack, null);
+    }
+  });
+}
+
 module.exports = {
-  getMovementTypeslist
+  getMovementTypeslist,
+  addNewOfice,
 }

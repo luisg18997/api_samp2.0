f1d995851bcf86beb7811e48c5c8d0ecbccf6f6f
lgonzalez <luis@ezgmz.com>
2018-11-12 19:00:43 -0400

    update controller not finish

--- controllers/formDataController.js
+++ controllers/formDataController.js
@@ -21,6 +21,37 @@ const getAllMovementTypeslist = (req, res) => {
   }
 };
 
+const getAllMovementTypeslist = (req, res) => {
+  try {
+    if (Object.keys(req.body).length !== 3) {
+      debug('request bad params not received');
+      result.parambad = 'request bad';
+      res.status(400).send(result);
+    } else {
+      const facultyID = req.body.param_faculty_id;
+      const facultyCode = req.body.param_faculty_code;
+      const facultyName = req.body.param_faculty_name;
+      const userID = req.body.param_user_id;
+      const facultyIsActive = req.body.param_faculty_is_active;
+      const facultyIsDeleted = req.body.param_faculty_is_deleted;
+
+      formDataModel.addNewFormOfice(facultyID, facultyCode, facultyName, userID,
+        facultyIsActive, facultyIsDeleted, (err, facultyUpdate) => {
+          if (err) {
+            result.messageError = err;
+            res.status(404).send(result);
+          } else {
+            debug('FacultyController: ', facultyUpdate);
+            res.send(facultyUpdate);
+          }
+        });
+    }
+  } catch (e) {
+    debug('error: ', e);
+    res.status(500).send(e);
+  }
+};
+
 module.exports = {
 
   getAllMovementTypeslist,

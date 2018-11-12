048472b876e9918f5bee544ecded82fc2dbb4208
lgonzalez <luis@ezgmz.com>
2018-11-12 13:28:20 -0400

    add new mode insert form mov peronsal

--- models/formDataModel.js
+++ models/formDataModel.js
@@ -23,8 +23,29 @@ const getMovementTypeslist = (callback) => {
   });
 };
 
-const addNewOfice = (employee, formOfice, callback) => {
-  const query = util.format('SELECT form_data.employee_form_ofice_insert_complete() as result;');
+const addNewFormOfice = (employee, formOfice, userID, callback) => {
+  const query = util.format("SELECT form_data.employee_form_ofice_insert_complete(param_employee_json := '%j', param_form_ofice_json := 'j', param_user_id := %d) as result;",
+    employee, formOfice, userID);
+  const data = {};
+  return pool.query(query, (err, res) => {
+ if (!err) {
+      debug('res.rows: ', res.rows[0].result.length);
+      if ((res.rowCount !== 0) && (res.rows[0].result != null)) {
+        debug('result obtain rowCount: ', res.rowCount);
+        callback(false, res.rows[0].result);
+      } else {
+        data.result = 'not found';
+        callback(false, data);
+      }
+    } else {
+      callback(err.stack, null);
+    }
+  });
+}
+
+const addNewFormMovPeronsal = (employee, formMovPeronsal,, userID callback) => {
+  const query = util.format("SELECT form_data.employee_form_movement_personal_insert_complete(param_employee_json := '%j', param_form_mov_per_json := 'j', param_user_id := %d) as result;",
+    employee, formOfice, userID);
   const data = {};
   return pool.query(query, (err, res) => {
  if (!err) {
@@ -44,5 +65,6 @@ const addNewOfice = (employee, formOfice, callback) => {
 
 module.exports = {
   getMovementTypeslist,
-  addNewOfice,
+  addNewFormOfice,
+  addNewFormMovPeronsal,
 }

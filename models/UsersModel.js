f784d2e9d2b606d19ca16c8ef9bd6d6f0ab3abc0
luis <luisgonzalez18997@gmail.com>
2018-12-30 21:58:08 -0400

    update model of user correction in the param of send

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -143,16 +143,47 @@ const login = (email, password, callback) => {
   const data = {};
   return pool.query(query, (err, res) => {
     if (!err) {
-      const passHash = res.rows[0].result[0].password;
       if ((res.rowCount !== 0) && (res.rows[0].result != null)) {
         debug('res.rows: ', res.rows[0].result.length);
+        debug(res.rows[0].result);
+        const passHash = res.rows[0].result.password;
         const compare = bcrypt.compareSync(password, passHash);
         if (compare) {
           debug('result obtain rowCount: ', res.rowCount);
-          data.name = res.rows[0].result[0].name;
-          data.surname = res.rows[0].result[0].surname;
-          data.email = res.rows[0].result[0].email;
-          data.ubication_id = res.rows[0].result[0].ubication_id;
+          data.name = res.rows[0].result.name;
+          data.email = res.rows[0].result.email;
+          data.ubicationId = res.rows[0].result.ubication_id;
+          data.ubication = res.rows[0].result.ubication;
+          data.isActive = res.rows[0].result.is_active;
+          data.isDeleted = res.rows[0].result.is_deleted;
+          if (res.rows[0].result.role_id === null) {
+            data.rolId = 0;
+          } else {
+            data.rolId = res.rows[0].result.role_id;
+          }
+          data.rol = res.rows[0].result.rol;
+          if (res.rows[0].result.school_id === null) {
+            data.schoolID = 0;
+          } else {
+            data.schoolID = res.rows[0].result.school_id;
+          }
+          if (res.rows[0].result.institute_id === null) {
+            data.instituteID = 0;
+          } else {
+            data.instituteID = res.rows[0].result.institute_id;
+          }
+          if (res.rows[0].result.coordination_id === null) {
+            data.coordinationID = 0;
+          } else {
+            data.coordinationID = res.rows[0].result.coordination_id;
+          }
+          if (res.rows[0].result.question_id === null) {
+            data.questionID = 0;
+          } else {
+            data.questionID = res.rows[0].result.question_id;
+          }
+          data.question = res.rows[0].result.question;
+          data.answer = res.rows[0].result.answer;
           callback(false, data);
         } else {
           data.result = '1';
